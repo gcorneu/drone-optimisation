7fbe99d1dc4ea2119779271fb37962dbc04cd3e5
tt24 <tt24@st-andrews.ac.uk>
2016-02-11 19:58:56 +0000

    Reading input

--- src/org/hashcode/main/IMap.java
+++ src/org/hashcode/main/IMap.java
@@ -1,10 +1,12 @@
 package org.hashcode.main;
 
+import java.util.ArrayList;
 import java.util.List;
 
 interface IMap {
     public int getWidth();
     public int getHeight();
+    public void setProducts(ArrayList<IProduct> products);
     public List<IWarehouse> getWarehouses();
     public List<IDrone> getDrones();
     public List<IOrder> getOrders();
---
+++ src/org/hashcode/main/Product.java
@@ -0,0 +1,14 @@
+package org.hashcode.main;
+
+public class Product implements IProduct{
+	private int weight;
+	public Product(int weight) {
+		this.weight = weight;
+	}
+	@Override
+	public int getWeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
